233b3d3c58c155f37a35aa7e4cfce7734648b88c
thewhistledev <firewallvmt@gmail.com>
2023-09-07 19:20:11 +0100

    Add project files.

---
+++ SignatureChecker/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+
+namespace SignatureChecker
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Usage: SignatureChecker <target_file_path>");
+                return;
+            }
+
+            var targetPath = args[0];
+            var signatures = LoadSignaturesFromFile();
+
+            if (Path.GetExtension(targetPath) == ".zip")
+            {
+                using (var zip = ZipFile.OpenRead(targetPath))
+                {
+                    foreach (var entry in zip.Entries)
+                    {
+                        using (var stream = new MemoryStream())
+                        {
+                            entry.Open().CopyTo(stream);
+                            CheckStreamForSignatures(stream.ToArray(), signatures, entry.Name);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                var data = File.ReadAllBytes(targetPath);
+                CheckStreamForSignatures(data, signatures, Path.GetFileName(targetPath));
+            }
+        }
+
+        private static void CheckStreamForSignatures(byte[] data, Dictionary<string, Dictionary<string, List<string>>> signatures, string filename)
+        {
+            var startIndex = data.Length > 2 && data[0] == 0x4D && data[1] == 0x5A ? 512 : 0;
+            var sliceSize = (data.Length - startIndex) / 12;
+            var detectedSignatures = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < 12; i++)
+            {
+                var segment = new byte[sliceSize];
+                Array.Copy(data, startIndex + (i * sliceSize), segment, 0, sliceSize);
+                var signature = GenerateSignature(segment);
+
+                foreach (var virusFamily in signatures)
+                {
+                    foreach (var uniqueName in virusFamily.Value)
+                    {
+                        if (uniqueName.Value.Contains(signature))
+                        {
+                            if (!detectedSignatures.ContainsKey(virusFamily.Key))
+                            {
+                                detectedSignatures[virusFamily.Key] = new List<string>();
+                            }
+                            detectedSignatures[virusFamily.Key].Add(uniqueName.Key);
+                        }
+                    }
+                }
+            }
+
+            DisplayDetectionSummary(filename, detectedSignatures);
+        }
+
+        private static void DisplayDetectionSummary(string filename, Dictionary<string, List<string>> detectedSignatures)
+        {
+            var output = new StringBuilder();
+            output.AppendLine($"Analysis for {filename}:");
+            output.AppendLine(new string('-', 50));
+
+            if (detectedSignatures.Any())
+            {
+                output.AppendLine("Potential malware detected:");
+                foreach (var detection in detectedSignatures)
+                {
+                    output.AppendLine($"\nType: {detection.Key}");
+                    foreach (var uniqueName in detection.Value.Distinct())
+                    {
+                        output.AppendLine($" - Signature: {uniqueName}");
+                    }
+                }
+                output.AppendLine($"\nTotal Types Detected: {detectedSignatures.Count}");
+                output.AppendLine($"Total Signatures Detected: {detectedSignatures.Sum(d => d.Value.Distinct().Count())}");
+            }
+            else
+            {
+                output.AppendLine("No malware signatures detected.");
+            }
+
+            Console.WriteLine(output.ToString());
+        }
+
+        private static string GenerateSignature(byte[] data)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return BitConverter.ToString(sha256.ComputeHash(data)).Replace("-", "");
+            }
+        }
+
+        private static Dictionary<string, Dictionary<string, List<string>>> LoadSignaturesFromFile()
+        {
+            var filePath = "signatures.json";
+            if (File.Exists(filePath))
+            {
+                var data = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, List<string>>>>(data);
+            }
+            return new Dictionary<string, Dictionary<string, List<string>>>();
+        }
+    }
+}
---
+++ SignatureGenerator/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SignatureGenerator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: SignatureGenerator <directory_path> <virus_type>");
+                return;
+            }
+
+            var directoryPath = args[0];
+            var virusType = args[1];
+            var signatures = LoadSignaturesFromFile();
+
+            if (Directory.Exists(directoryPath))
+            {
+                ProcessDirectory(directoryPath, virusType, signatures);
+                File.WriteAllText("signatures.json", JsonConvert.SerializeObject(signatures, Formatting.Indented));
+            }
+            else
+            {
+                Console.WriteLine("Specified directory does not exist.");
+            }
+        }
+
+        private static void ProcessDirectory(string directoryPath, string virusType, Dictionary<string, Dictionary<string, List<string>>> signatures)
+        {
+            foreach (var file in Directory.GetFiles(directoryPath))
+            {
+                GenerateSignaturesForFile(file, virusType, signatures);
+            }
+
+            foreach (var subDirectory in Directory.GetDirectories(directoryPath))
+            {
+                ProcessDirectory(subDirectory, virusType, signatures);
+            }
+        }
+
+        private static void GenerateSignaturesForFile(string filePath, string virusType, Dictionary<string, Dictionary<string, List<string>>> signatures)
+        {
+            using (var stream = File.OpenRead(filePath))
+            {
+                var data = new byte[stream.Length];
+                stream.Read(data, 0, (int)stream.Length);
+                var startIndex = data.Length > 2 && data[0] == 0x4D && data[1] == 0x5A ? 512 : 0;
+                var sliceSize = (data.Length - startIndex) / 12;
+                List<string> generatedSignatures = new List<string>();
+
+                for (int i = 0; i < 12; i++)
+                {
+                    var segment = new byte[sliceSize];
+                    Array.Copy(data, startIndex + (i * sliceSize), segment, 0, sliceSize);
+                    generatedSignatures.Add(GenerateSignature(segment));
+                }
+
+                if (!signatures.ContainsKey(virusType))
+                {
+                    signatures[virusType] = new Dictionary<string, List<string>>();
+                }
+
+                var random = new Random();
+                var animals = new List<string> { "Lion", "Tiger", "Bear", "Eagle", "Shark", "Jaguar", "Panda", "Elephant", "Leopard", "Rhinoceros" };
+                var uniqueName = $"{animals[random.Next(animals.Count)]}!{virusType}";
+                signatures[virusType][uniqueName] = generatedSignatures;
+            }
+        }
+
+        private static string GenerateSignature(byte[] data)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                return BitConverter.ToString(sha256.ComputeHash(data)).Replace("-", "");
+            }
+        }
+
+        private static Dictionary<string, Dictionary<string, List<string>>> LoadSignaturesFromFile()
+        {
+            var filePath = "signatures.json";
+            if (File.Exists(filePath))
+            {
+                var data = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, List<string>>>>(data);
+            }
+            return new Dictionary<string, Dictionary<string, List<string>>>();
+        }
+    }
+}
